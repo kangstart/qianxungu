c1be967b285cf93a7e56a7c37bb711fe5be5407c
zhuohuiHuang <1007654471@qq.com>
2019-07-20 11:51:41 +0800

    '接口文档'

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,22 @@
 import Vue from 'vue'
 var api = {
+    //系统管理
     signin:'/signin',
-    checkDynamicCode:'/checkDynamicCode'
+    checkDynamicCode:'/checkDynamicCode',
+    // 角色列表
+    findRoles:'/system/role/findRoles',
+    deleteRole:'/system/role/deleteRole',
+    addNewRole:'/system/role/addNewRole',
+    updateRole:'/system/role/updateRole',
+    //用户
+    findAdmins:'/system/admin/findAdmins',
+    addNewAdmin:'/system/admin/addNewAdmin',
+    deleteAdmin:'/system/admin/deleteAdmin',
+    //权限
+    findPermission:'/system/permission/findPermission',
+    addNewPermission:'/system/permission/addNewPermission',
+    deletePermission:'/system/permission/deletePermission'
+
 }
 Vue.prototype.$api = api;
 export default{api}
